e7affe1f2c259d59ac7a68a487931f7bf2291b93
Bradley Rispone <bradrispone@gmail.com>
2023-03-24 03:39:58 -0500

    Add validation and styling for password and confirm password match

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 document.querySelector("#user-info-form").addEventListener("submit", function(event) {
-    alert("Form Submitted");
     const password = document.querySelector("#password");
     const confirmPassword = document.querySelector("#confirm-password");
+    const messageText = document.querySelector("#pass-error-message");
 
-    if (password.value === confirmPassword.value) {
-        alert("Passwords match");
+    if (password.value != confirmPassword.value) {
+        messageText.innerText = "PASSWORDS DO NOT MATCH";
+        password.setCustomValidity("Passwords do not match.");
+        confirmPassword.setCustomValidity("Passwords do not match.");
+        event.preventDefault();
+    } else {
+        messageText.innerText= "";
     }
+});
+
+document.querySelector("#password").addEventListener("input", function() {
+    this.setCustomValidity("");
+    document.querySelector("#pass-error-message").innerText = "";
+});
+
+document.querySelector("#confirm-password").addEventListener("input", function() {
+    this.setCustomValidity("");
+    document.querySelector("#pass-error-message").innerText = "";
 });
